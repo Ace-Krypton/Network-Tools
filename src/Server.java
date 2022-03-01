da6bff0b52bfde394d47658e13c6a419e488481e
ace-krypton <ramizna@code.edu.az>
2022-03-01 19:02:59 +0100

    Closed All Streams Successfully

--- src/Server.java
+++ src/Server.java
@@ -10,13 +10,24 @@ public class Server {
         //Listening on port: 9090
         ServerSocket serverSocket = new ServerSocket(9090);
         System.out.println("Waiting for clients");
+
         //Listens until the connection is established
         Socket socket = serverSocket.accept();
+
         //Prints Data and autoFlush mode is on
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         out.println("Hello Client");
+        
         //It is for reading the data
         BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+        //For the client input
+        String clientInput = input.readLine();
+        System.out.println(clientInput);
+
+        //Closing the Streams
+        input.close();
+        out.close();
+        socket.close();
     }
 }
