874f596021ef5e82ab39ec83319fb64409bbf809
ace-krypton <ramizna@code.edu.az>
2022-03-01 19:00:22 +0100

    Added Printing and Reading Functionalities

--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(9090); //Listening on port: 9090
+        //Listening on port: 9090
+        ServerSocket serverSocket = new ServerSocket(9090);
         System.out.println("Waiting for clients");
-        Socket socket = serverSocket.accept();  //Listens until the connection is established
+        //Listens until the connection is established
+        Socket socket = serverSocket.accept();
+        //Prints Data and autoFlush mode is on
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println("Hello Client");
+        //It is for reading the data
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
     }
 }
