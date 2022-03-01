842aa8ae46d556a11634b5bf715dbd1403ef1cb4
ace-krypton <ramizna@code.edu.az>
2022-03-01 18:34:30 +0100

    Added Socket Server

--- src/LocalPortScanner.java
+++ src/LocalPortScanner.java
@@ -1,3 +1,12 @@
-public class LocalPortScanner {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+public class LocalPortScanner {
+    public static void main(String[] args) throws IOException {
+        try {
+            ServerSocket server = new ServerSocket(9090);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
