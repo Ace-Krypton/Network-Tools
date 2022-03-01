92f33d723c853defe0f7715c84ab143388da6f51
ace-krypton <ramizna@code.edu.az>
2022-03-01 18:40:51 +0100

    Added port numbers, and loop

--- src/LocalPortScanner.java
+++ src/LocalPortScanner.java
@@ -3,10 +3,17 @@ import java.net.ServerSocket;
 
 public class LocalPortScanner {
     public static void main(String[] args) throws IOException {
-        try {
-            ServerSocket server = new ServerSocket(9090);
-        } catch (IOException e) {
-            e.printStackTrace();
+        int port = 1;   //Starts with 1
+
+        while (port <= 65535) { //Ends with 65535
+            //Checks if it is used or not
+            try {
+                ServerSocket server = new ServerSocket(port);
+            } catch (IOException e) {   //For the used ports it gives us this message
+                System.out.println("Port is already been used for other application");
+                e.printStackTrace();
+            }
+            port++; //Everytime we are increasing the value of the port by 1
         }
     }
 }
