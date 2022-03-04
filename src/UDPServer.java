faff2780bebf1f4128d577c758f5f2bead8f69c4
ace-krypton <ramizna@code.edu.az>
2022-03-04 20:10:38 +0100

    Small Changes

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,4 +1,6 @@
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.Inet4Address;
 import java.net.SocketException;
 
 public class UDPServer {
@@ -9,7 +11,16 @@ public class UDPServer {
             byte[] sendData = new byte[1024];
 
             while (true) {
-                
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+                String sentence = new String(receivePacket.getData());
+                System.out.println("Received -> " + sentence);
+                String data = "Hello Client";
+                sendData = data.getBytes();
+                Inet4Address clientIPAddress = (Inet4Address) receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientIPAddress, clientPort);
+                socket.send(sendPacket);
             }
         } catch (Exception e) {
             e.printStackTrace();
