ffa1b1f94e3bf849dfe7ed1ddf714bc67b7cdc16
ace-krypton <ramizna@code.edu.az>
2022-03-04 21:26:03 +0100

    Added TimeOut to the UDP Client

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -7,7 +7,7 @@ public class UDPClient {
             byte[] sendData;    //Store outgoing data
             byte[] receiveData = new byte[1024]; //Store incoming data
             //Amount of data = 65535 - 20 (IP Header) - 8 (UDP Header) = 65508
-
+            clientSocket.setSoTimeout(3000);
             Inet4Address serverAddress = (Inet4Address) Inet4Address.getByName("localhost");
 
             String stringSendData = "Hello Server";
